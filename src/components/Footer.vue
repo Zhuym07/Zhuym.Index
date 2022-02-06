3ccca0e36af0575e75888e7bbd2334d7fa0adb87
Zhuym07 <34023479+Zhuym07@users.noreply.github.com>
2022-02-06 18:56:30 +0800

    Fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,9 @@
     <div class="my-12 px-4 text-caption" style="opacity: .75">
         <b>Zhuym's Home</b>
         <br />Created by Zhuym with ♥ since 2022.
-        <br /><a href="https://icp.gov.moe/?keyword=20220114" target="_blank">萌ICP备20220114号</a>
-        <br /><a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">赣ICP备20004742号-1</a>
-        <br /><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36010602000076" target="_blank">赣公网安备36010602000076号</a>
+        <br />萌ICP备20220114号
+        <br />赣ICP备20004742号-1
+        <br />赣公网安备36010602000076号
         <br />
         <br />&copy; 2022 Zhuym. Template by fsky7.
         <v-dialog v-model="dialog" max-width="768px" :dark="dark">
